696da00cdc35e02062e1cced79b7db898cd8d5d3
malayat <alejandro.ayala@solmedia.ec>
2018-01-02 23:19:46 -0500

    Add generics examples

---
+++ src/main/kotlin/Generics/TypedClass.kt
@@ -0,0 +1,43 @@
+package Generics
+
+class TypedClass<T : Any>(parameter: T) {
+    val t: T = parameter
+}
+
+class TypedClass2<T> {
+    fun doSomething() {
+    }
+}
+
+class TypedClass3<out T> {
+    fun doSomething() {
+
+    }
+}
+
+fun main(args: Array<String>) {
+    val t1 = TypedClass<String>("Hello world")
+    /*Type argument is not within its bounds.
+    Expected:
+    Any
+    Found:
+    String?*/
+    val t2 = TypedClass<String?>(null) //not compile
+
+    val tc1 = TypedClass2<String>()
+    /*Type mismatch.
+      Required:
+      TypedClass2<Any>
+      Found:
+      TypedClass2<String>*/
+    val tc2: TypedClass2<Any> = tc1 //not compile
+
+    val tc3 = TypedClass3<String>()
+    val tc4: TypedClass3<Any> = tc3 //ok
+
+}
+
+//also allowed in funtions
+fun <T> typedFuntion(item: T): List<T> {
+    return emptyList()
+}
