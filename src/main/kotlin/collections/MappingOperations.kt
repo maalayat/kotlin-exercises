6c9fe9c2846d295b9e67116a8d10cc64579215f4
Alejandro Ayala Ati <maaa220817@sri.ad>
2018-05-11 09:06:51 -0500

    Flatmap explanation is improved

--- src/main/kotlin/collections/MappingOperations.kt
+++ src/main/kotlin/collections/MappingOperations.kt
@@ -8,8 +8,8 @@ fun main(args: Array<String>) {
     println(list.flatMap { listOf(it, it + 1) }) //: [1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7]
     // se necesita [1a, 1b, 2a, 2b, 3a, 3b, 4a, 4b]
     // si usamos dos map?
-    println(list.map { m1 -> list2.map { m2 -> "$m1$m2" } }) //: [1a, 1b], [2a, 2b], [3a, 3b], [4a, 4b], [5a, 5b], [6a, 6b]
-    println(list.flatMap { m1 -> list2.map { m2 ->  "$m1$m2"} })
+    println(list.map { m1 -> list2.map { m2 -> "$m1$m2" } }) //:    [[1a, 1b], [2a, 2b], [3a, 3b], [4a, 4b], [5a, 5b], [6a, 6b]]
+    println(list.flatMap { m1 -> list2.map { m2 ->  "$m1$m2"} }) //:[1a, 1b, 2a, 2b, 3a, 3b, 4a, 4b, 5a, 5b, 6a, 6b]
 
     /*************************** groupBy ***************************/
     //Devuelve un mapa de los elementos de la colección original agrupados por el resultado de la función dada
