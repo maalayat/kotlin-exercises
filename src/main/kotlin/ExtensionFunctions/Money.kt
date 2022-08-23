fc823f94f761141f91eed382970b265e3b9227ec
Alejandro Ayala <mayalaat@pichincha.com>
2022-08-23 17:25:24 -0500

    Remove args param

--- src/main/kotlin/ExtensionFunctions/Money.kt
+++ src/main/kotlin/ExtensionFunctions/Money.kt
@@ -4,13 +4,16 @@ import java.math.BigDecimal
 
 data class Money(val amount: BigDecimal, val currency: String)
 
-fun main(args: Array<String>) {
+fun main() {
     val tickets = Money(BigDecimal(100), "$")
+    println(tickets)
     val bus = Money(BigDecimal(120), "EUR")
+    println(bus)
 
     val tickets2 = Money(100.bd, "$")
+    println(tickets2)
     val bus2 = Money(120.bd, "EUR")
-
+    println(bus2)
 }
 
 private val Int.bd: BigDecimal
